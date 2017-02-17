d9503b825c1db4975e0097862cad32583898f970
Jeremy Kennedy <me@jeremyk.net>
2017-02-17 16:07:13 -0500

    move styles to beginning of file, quick reformat with "prettier"

--- speedtest.js
+++ speedtest.js
@@ -1,53 +1,61 @@
+var fs = require("fs");
 
-var fs = require('fs');
-
-var express = require('express');
-var bodyParser = require('body-parser')
+var express = require("express");
+var bodyParser = require("body-parser");
 
 var app = express();
 
-app.use(bodyParser.raw({inflate:false,type:"*/*",limit:1024*1024*1024*10}));
+app.use(
+  bodyParser.raw({
+    inflate: false,
+    type: "*/*",
+    limit: 1024 * 1024 * 1024 * 10
+  })
+);
 
-app.use(function(err, req, res, next){
-  	console.log("error: "+err.stack);
-  	res.status(500).send('Something broke!');
+app.use(function(err, req, res, next) {
+  console.log("error: " + err.stack);
+  res.status(500).send("Something broke!");
 });
 
-app.get('/',function(req,res) {
-    res.sendFile(__dirname+"/index.html");
-    console.log("Incoming Request from "+req.connection.remoteAddress);
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/index.html");
+  console.log("Incoming Request from " + req.connection.remoteAddress);
 });
 
-app.get('/blob/:size',function (req,res) {
-    var size=req.params.size;
-    if (!size || size<=0) size=1;
-    for (var i=0;i<size;i++) res.write(blob);
-    res.end();
+app.get("/blob/:size", function(req, res) {
+  var size = req.params.size;
+  if (!size || size <= 0) size = 1;
+  for (var i = 0; i < size; i++) res.write(blob);
+  res.end();
 });
 
-app.post('/blob/',function (req,res) {
-    res.send("OK: "+req.body.length);
+app.post("/blob/", function(req, res) {
+  res.send("OK: " + req.body.length);
 });
 
-var blob=new Buffer(1024*1024);
+var blob = new Buffer(1024 * 1024);
 
-  	// ---------- initialize random blob 
-fs.open("/dev/urandom","r",function (err,fd) {
-  fs.readSync(fd,blob,0,1024*1024,0);
+// ---------- initialize random blob
+fs.open("/dev/urandom", "r", function(err, fd) {
+  fs.readSync(fd, blob, 0, 1024 * 1024, 0);
 });
 
-var listen="0.0.0.0";
-var port="8081";
+var listen = "0.0.0.0";
+var port = "8081";
 
-opt=require('node-getopt').create([
-        ["l","bindaddress=ARG","Listen to this IP (any)"],
-        ["p","port=ARG","Listen to this TCP Port (8081)"],
-        ['h','help','display this help']
-]).bindHelp().parseSystem();
+opt = require("node-getopt")
+  .create([
+    ["l", "bindaddress=ARG", "Listen to this IP (any)"],
+    ["p", "port=ARG", "Listen to this TCP Port (8081)"],
+    ["h", "help", "display this help"]
+  ])
+  .bindHelp()
+  .parseSystem();
 
-if (opt.options.bindaddress) listen=opt.options.bindaddress;
-if (opt.options.port) port=opt.options.port;
+if (opt.options.bindaddress) listen = opt.options.bindaddress;
+if (opt.options.port) port = opt.options.port;
 
-var server = app.listen(port,listen, function() {
-    console.log('Listening on %s:%d', listen, server.address().port);
+var server = app.listen(port, listen, function() {
+  console.log("Listening on %s:%d", listen, server.address().port);
 });
